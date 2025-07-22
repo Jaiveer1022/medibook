045cf5cd77ae0b3333fce37f6b1029975cf2067e
Jaiveeer <jaiveeryadav75033@gmail.com>
2025-07-22 17:26:23 +0530

    project file

---
+++ src/main/java/com/dac/cdac/medibook/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dac.cdac.medibook;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MedibookApplication.class);
+	}
+
+}
